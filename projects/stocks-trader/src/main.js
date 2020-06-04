135478cb32e4b06ce2eabee691821cd1bf82d308
Rohit Gutal <rohitgutal@rohits-mbp.home>
2020-06-04 18:12:25 +0200

    Update remove unused vues from main.js file

--- projects/stocks-trader/src/main.js
+++ projects/stocks-trader/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Vuex from 'vuex'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
