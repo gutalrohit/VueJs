8a33ae3eff93fd326f38007132a15fb55b9a4376
Rohit Gutal <rohitgutal@rohits-mbp.home>
2020-08-18 21:19:52 +0200

    Layout alignment to align center on mobile devices

--- Exercise/vue-router-practice/src/App.vue
+++ Exercise/vue-router-practice/src/App.vue
@@ -28,47 +28,6 @@ export default {
 }
 
 
-    .slide-enter {
-        opacity: 0;
-        /*transform: translateY(20px);*/
-    }
-
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-        transition: opacity .5s;
-    }
-
-    .slide-leave {
-
-    }
-
-    .slide-leave-active {
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1s;
-        opacity: 0;
-        position: absolute;
-    }
-
-    .slide-move {
-        transition: transform 1s;
-    }
-
-    @keyframes slide-in {
-        from {
-            transform: translateY(20px);
-        }
-        to {
-            transform: translateY(0);
-        }
-    }
-
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-        }
-        to {
-            transform: translateY(20px);
-        }
-    }
+    
 
 </style>
